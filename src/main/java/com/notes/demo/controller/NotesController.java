aca3ecdaba5f4a5b0c0941338661f359162f3963
Abdulrahman Ghazwani <d.m1999y@gmail.com>
2026-01-19 16:19:07 +0300

    Add WebConfig for CORS and allow Angular localhost

--- src/main/java/com/notes/demo/controller/NotesController.java
+++ src/main/java/com/notes/demo/controller/NotesController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notes")
